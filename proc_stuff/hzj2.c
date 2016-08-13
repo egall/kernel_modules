26acb63ee48408792eb630db379e923e6a41a164
Erik Steggall <eqsteggall@gmail.com>
2016-08-12 18:25:52 -0700

    Completed challenge asg of chapter 13 in crashcourse for creating hz and jiffies under proc

--- proc_stuff/hzj2.c
+++ proc_stuff/hzj2.c
@@ -9,9 +9,16 @@
 static int
 hz_show(struct seq_file *m, void *v)
 {
-    seq_printf(m, "jiffies: %llu\nhz: %d\n", \
-              (unsigned long long) get_jiffies_64(),
-                                   HZ);
+    seq_printf(m, "hz: %d\n", HZ);
+
+    return 0;
+}
+
+static int
+jiffies_show(struct seq_file *m, void *v)
+{
+    seq_printf(m, "jiffies: %llu\n", \
+              (unsigned long long) get_jiffies_64());
 
     return 0;
 }
@@ -22,9 +29,24 @@ hz_open(struct inode *inode, struct file *file)
     return single_open(file, hz_show, NULL);
 }
 
+
+static int
+jiffies_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, jiffies_show, NULL); 
+}
+
 static const struct file_operations hz_fops = {
     .owner      = THIS_MODULE,
     .open       = hz_open,
+    .read       = seq_read,
+    .llseek     = seq_lseek,
+    .release    = single_release,
+};
+
+static const struct file_operations jiffies_fops = {
+    .owner      = THIS_MODULE,
+    .open       = jiffies_open,
     .read       = seq_read,
     .llseek     = seq_lseek,
     .release    = single_release,
@@ -34,7 +56,8 @@ static int __init
 hz_init(void)
 {
     printk(KERN_INFO "Loading hz module, HZ = %d.\n", HZ);
-    proc_create("hzj", 0, NULL, &hz_fops);
+    proc_create("hz", 0, NULL, &hz_fops);
+    proc_create("jiffies", 0, NULL, &jiffies_fops);
     return 0;
 }
 
@@ -42,6 +65,7 @@ static void __exit
 hz_exit(void)
 {
     remove_proc_entry("hz", NULL);
+    remove_proc_entry("jiffies", NULL);
     printk(KERN_INFO "Unloading hz module.\n");
 }
 
